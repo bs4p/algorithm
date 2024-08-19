9a5d103ee9611008a2d791a52b4b4f7a82afe14c
bs4p <bambang.s4p@gmail.com>
2024-08-19 19:39:54 +0700

    create and add insertion-sort.txt

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,21 +1,19 @@
 function insertionSort([...data]) {
-  let lsi = 0;
-
   for (let i = 1; i < data.length; i++) {
-    let iValue = data[i];
+    let iVal = data[i];
     let j = i - 1;
 
-    // console.log(`iValue : ${iValue}`);
+    // console.log(`iVal : ${iVal}`);
     // console.log(`(i : ${i}) | (lsi : ${lsi}) | | Check Orders : `);
 
-    while (j >= 0 && iValue < data[j]) {
+    while (j >= 0 && iVal < data[j]) {
       // console.log(`### j : ${j} ###`);
-      // console.log(`data[${j}] : ${data[j]} | iValue(${i}) : ${iValue}`);
+      // console.log(`data[${j}] : ${data[j]} | iVal(${i}) : ${iVal}`);
       data[j + 1] = data[j];
       j--;
     }
     // console.log('-------------------- FINAL j : ' + j);
-    data[j + 1] = iValue;
+    data[j + 1] = iVal;
   }
 
   // console.log(`\n~~~~~~~~~~ END ~~~~~~~~~~`);
