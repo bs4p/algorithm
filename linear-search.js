d4656d1794f1a4115137937a08b0f87ed484d2f0
bs4p <bambang.s4p@gmail.com>
2024-08-22 19:33:12 +0700

    rename parameter (search -> target) & add return value if target not found

--- linear-search.js
+++ linear-search.js
@@ -1,10 +1,11 @@
-function linearSearch(search, [...data]) {
+function linearSearch(target, [...data]) {
   for (let i = 0; i < data.length; i++) {
-    if (data[i] !== search) continue;
+    if (data[i] !== target) continue;
     return i;
   }
+  return -1;
 }
 
 const data = [4, 0, 5, 2, 1];
-const searchResult = linearSearch(5, data);
+const searchResult = linearSearch(9, data);
 console.log(searchResult);
